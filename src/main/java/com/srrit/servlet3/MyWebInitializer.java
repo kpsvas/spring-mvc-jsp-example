21ca5d170dd8862ba0a9c5d7b36d5a3bf51c9e49
Srinivas Komatipally <kpsvas@gmail.com>
2023-11-16 19:31:12 -0500

    Add files via upload

---
+++ src/main/java/com/srrit/servlet3/MyWebInitializer.java
@@ -0,0 +1,25 @@
+package com.srrit.servlet3;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import com.srrit.config.SpringWebConfig;
+
+public class MyWebInitializer extends
+		AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] { SpringWebConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+}
